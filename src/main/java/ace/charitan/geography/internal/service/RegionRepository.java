47f8650dc1a35fc63c63d478fed6567d184d7a1e
Ngo Quoc Binh <ngobinh0312@gmail.com>
2024-12-19 23:50:23 +0700

    add repo

---
+++ src/main/java/ace/charitan/geography/internal/service/RegionRepository.java
@@ -0,0 +1,7 @@
+package ace.charitan.geography.internal.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface RegionRepository extends JpaRepository<Region, Long> {
+
+}
