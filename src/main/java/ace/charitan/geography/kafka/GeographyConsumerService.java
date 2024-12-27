f62c524b34b21f872347dc9395d33296541709c9
Ngo Quoc Binh <ngobinh0312@gmail.com>
2024-12-27 13:42:11 +0700

    add kafka to geo (basic)

---
+++ src/main/java/ace/charitan/geography/kafka/GeographyConsumerService.java
@@ -0,0 +1,30 @@
+package ace.charitan.geography.kafka;
+
+import java.util.Map;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ace.charitan.geography.kafka.topic.ProjectProducerTopic;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+class GeographyConsumerService {
+
+    private final ObjectMapper objectMapper;
+
+    @KafkaListener(topics = ProjectProducerTopic.PROJECT_GEOGRAPHY_GET_COUNTRY_BY_ISO_CODE, groupId = "project-id")
+    void handleGetCountryByIsoCode(String message) {
+        try {
+            Map<String, Object> getCountryByIsoCodeRequestDto = objectMapper.readValue(message, Map.class);
+            System.out.println(getCountryByIsoCodeRequestDto.get("isoCode"));
+        } catch (Exception e) {
+            System.err.println("Failed to process donor created event: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
