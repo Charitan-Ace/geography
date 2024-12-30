907dd2e6ceffad02ef846e5134654e1d368faa49
Ngo Quoc Binh <ngobinh0312@gmail.com>
2024-12-30 18:26:11 +0700

    add response

--- src/main/java/ace/charitan/geography/kafka/GeographyConsumerService.java
+++ src/main/java/ace/charitan/geography/kafka/GeographyConsumerService.java
@@ -3,11 +3,14 @@ package ace.charitan.geography.kafka;
 import java.util.Map;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import ace.charitan.geography.kafka.dto.GetCountryByIsoCode.GetCountryByIsoCodeRequestDto;
+import ace.charitan.common.dto.TestKafkaMessageDto;
+import ace.charitan.common.dto.country.GetCountryByIsoCode.GetCountryByIsoCodeRequestDto;
 import ace.charitan.geography.kafka.topic.ProjectProducerTopic;
 import lombok.RequiredArgsConstructor;
 
@@ -15,18 +18,16 @@ import lombok.RequiredArgsConstructor;
 @Component
 class GeographyConsumerService {
 
-    private final ObjectMapper objectMapper;
+    // private final ObjectMapper objectMapper;
 
-    @KafkaListener(topics = ProjectProducerTopic.PROJECT_GEOGRAPHY_GET_COUNTRY_BY_ISO_CODE, groupId = "project-id")
-    void handleGetCountryByIsoCode(GetCountryByIsoCodeRequestDto dto) {
-        try {
-            System.out.println(dto);
-            // Map<String, Object> getCountryByIsoCodeRequestDto = objectMapper.readValue(message, Map.class);
-            // System.out.println(getCountryByIsoCodeRequestDto.get("isoCode"));
-        } catch (Exception e) {
-            System.err.println("Failed to process donor created event: " + e.getMessage());
-            e.printStackTrace();
-        }
+    @KafkaListener(topics = ProjectProducerTopic.PROJECT_GEOGRAPHY_GET_COUNTRY_BY_ISO_CODE, groupId = "geography-id")
+    @SendTo(KafkaHeaders.REPLY_TOPIC)
+    TestKafkaMessageDto handleGetCountryByIsoCode(GetCountryByIsoCodeRequestDto dto) {
+        System.out.println(dto);
+        // Map<String, Object> getCountryByIsoCodeRequestDto =
+        // objectMapper.readValue(message, Map.class);
+        // System.out.println(getCountryByIsoCodeRequestDto.get("isoCode"));
+        return new TestKafkaMessageDto("afds", "fdfds");
     }
 
 }
